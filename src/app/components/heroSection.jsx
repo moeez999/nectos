a18210c5cfa762f06e8cfc4ba7fcfdf348826881
moeez999 <moeezraza69@gmail.com>
2024-12-27 20:39:02 +0500

    update

--- src/app/components/heroSection.jsx
+++ src/app/components/heroSection.jsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
       <div className="pt-4">
         <Navbar />
       </div>
-      <div className="absolute top-24 left-1/2 transform -translate-x-1/2 z-10">
+      <div className="absolute top-24 left-1/2 transform -translate-x-1/2 z-10 hidden lg:block">
         <img src="../img/hero-arrow.png" alt="" />
       </div>
       <div className="mx-auto max-w-7xl px-4 sm:px-6  md:flex md:items-center md:gap-x-10 md:px-8 relative">
